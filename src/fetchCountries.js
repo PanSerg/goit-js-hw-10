c5b8c20a3cc32a34253d48e007f92ef402580105
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-01-04 21:32:53 +0100

    task1

---
+++ src/fetchCountries.js
@@ -0,0 +1,13 @@
+
+export default function fetchCountries(name) {
+//    return fetch('https://restcountries.com/v3.1/name/united')
+//      .then(response => {
+//        return response.json();
+//      })
+//      .then(country => {
+//        console.log(country);
+//      })
+//      .catch(error => {
+//        console.log(error);
+//      });
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import './css/styles.css';
+import countryBase from './fetchCountries';
+console.log(countryBase);
+
+const refs = {
+  form: document.querySelector('#search-box'),
+  itemContainer: document.querySelector('.country-list'),
+};
 
 const DEBOUNCE_DELAY = 300;
+fetch('https://restcountries.com/v3.1/name/united')
+  .then(response => {
+    return response.json();
+  })
+  .then(country => {
+    console.log(country);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+refs.form.addEventListener('input', onInput);
+
+function onInput(evt) {
+    evt.preventDefault();
+    const { capital, population, languages,flags} = evt.currentTarget;
+    console.log(capital.value, population.value, languages.value, flags.value)
+        .then(data = console.log(data))
+        .catch(error = console.log(error));
+};
+
+
