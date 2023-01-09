ae6747efee3b7ecb5826ccded7e23f8b8c83c637
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-01-09 10:39:39 +0100

    task1.4

--- src/index.js
+++ src/index.js
@@ -39,9 +39,8 @@ function renderCountryContainer(countries) {
     return `<li class="country-list__item">
               <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
               <h2 class="country-info__name">${name.official}</h2>
-            </li >`
-})
-.join('')
+            </li>`
+}).join('')
   return markup
 }
 
